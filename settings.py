3fc8b2549cd24624b45d739e6482fd8422b6d4ca
cauchymike <michealakinkuotu73@gmail.com>
2023-09-13 05:23:17 +0100

    modified requirements.txt

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 import os
 
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '*********************')
